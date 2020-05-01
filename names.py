6102a69748a5964b5227d92fd99faaa42af9e8ec
dole <jimmyjohnrussel@gmail.com>
2020-05-01 20:11:44 +0100

    add GNOMED

--- names.py
+++ names.py
@@ -5,7 +5,7 @@ from pathlib import Path
 packfp = Path('./v4cpack.pk3').resolve()
 
 pack = zipfile.ZipFile(packfp)
-names = []
+karts = []
 for fname in pack.namelist():
     if fname[-1] == '/': continue
     if fname.split('/')[-1][:6] != 'S_SKIN': continue
@@ -19,15 +19,33 @@ for fname in pack.namelist():
         try: value = line[1].strip()
         except IndexError: continue
         props[key] = value
-    names.append(props['realname'])
+    karts.append({
+        'name': props['realname'],
+        'speed': max(min(int(props['kartspeed']), 9), 1),
+        'weight': max(min(int(props['kartweight']), 9), 1)
+    })
+pack.close()
 
-names.sort()
+karts = sorted(karts, key=lambda k: k['name'])
+
 with open('names.txt', 'w') as f:
-    for name in names:
-        f.write(name)
-        f.write('\n')
+    for kart in karts:
+        f.write(kart['name'] + '\n')
 
-pack.close()
-pack = zipfile.ZipFile(packfp, mode='a')
-pack.write('yuh.txt', arcname='hibiki/yuh.txt')
-pack.close()
+table = [[[] for j in range(9)] for i in range(9)]
+for kart in karts:
+    table[kart['speed'] - 1][kart['weight'] - 1].append(kart['name'])
+
+with open('table.csv', 'w') as f:
+    f.write('W/S')
+    for i in range(1,10):
+        f.write(f',Speed {i}')
+    f.write('\n')
+    for weight in range(1,10):
+        f.write(f'Weight {weight}')
+        for speed in range(1, 10):
+            f.write(',"')
+            for kart in table[speed - 1][weight - 1]:
+                f.write(kart + '\n')
+            f.write('"')
+        f.write('\n')
